1cea2eac2f1957cd2731fe1e441d3b38565db6a8
Amin Mozaffarin <a.mozaffarin@mozys.de>
2024-02-13 23:04:00 +0100

    initial release

--- scripts/content.js
+++ scripts/content.js
@@ -1,17 +1,34 @@
-setTimeout(() => {
+setTimeout(async () => {
     const el = document.querySelector('div.system-versionbar ul li > small:nth-child(2)');
+    const footer = document.getElementById('footer')
+
     // `document.querySelector` may return null if the selector doesn't match anything.
     if (el) {
-        if (!el.textContent) {
-            console.log({ textContent: el.textContent })
-            return console.log('S/N NOT FOUND')
-        }
-
-        console.log('S/N:', el.textContent || 'NOT FOUND')
         const serialNo = el.textContent
-        el.id = 'serial-no'
+        if (!serialNo) {
+            return console.log('S/N: NOT FOUND')
+        }
+        console.log('S/N:', serialNo)
 
-        const styleElem = document.head.appendChild(document.createElement("style"));
-        styleElem.innerHTML = `#serial-no:after {content: ''; background: yellow url('http://bwipjs-api.metafloor.com/?bcid=datamatrix&text=${serialNo}') no-repeat 8px 8px; background-size: 32px 32px; width: 48px; height: 48px; bottom: 32px; left: 0; position: absolute; display: inline-block;}`;
+        const options = await chrome.storage.sync.get()
+        let hScale = 1
+        if (options.barcodeType === 'code128') {
+            hScale = 0.2
+        }
+        
+        const imageWrapper = document.createElement('div')
+        imageWrapper.id = 'barcode-wrapper'
+
+        const img = new Image()
+        img.onload = function() {
+            const width = this.width
+            const height = Math.round(width * hScale)
+            const styleElem = document.head.appendChild(document.createElement("style"));
+            styleElem.innerHTML = `#barcode-wrapper { position: absolute; bottom: 30px; background: yellow; padding: 0.875rem; } #barcode-wrapper img { width: ${width}px; height: ${height}px; }`
+        }
+        img.src = `http://bwipjs-api.metafloor.com/?bcid=${options.barcodeType}&text=${serialNo}`
+        
+        imageWrapper.appendChild(img)
+        footer.appendChild(imageWrapper)
     }
 }, 1000)

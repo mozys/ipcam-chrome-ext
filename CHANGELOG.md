72b7e3794a8ef1673ae95b1783b42c2bb0c663d4
Amin Mozaffarin <a.mozaffarin@mozys.de>
2024-02-17 14:51:14 +0100

    rearrange test order and setup for offline setup

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## [2.0] - 2024-02-17
+### Added
+- Rearranged test order for test duration minimization
+- Option to select between (default) offline and online setup
+- Remove backend tests and licence activation in offline setup
+
+### Changed
+- Render barcode only on bis page
+- Store external dependencies locally for offline use
+- Icon color changed to MOZYS orange
--- scripts/options.js
+++ scripts/options.js
@@ -3,8 +3,11 @@ const saveOptions = () => {
   const barcodeType = document.getElementById('barcode-type').value
   console.log(barcodeType)
 
+  const setupMode = document.getElementById('setup-mode').value
+  console.log(setupMode)
+
   chrome.storage.sync.set(
-    { barcodeType: barcodeType },
+    { barcodeType, setupMode},
     () => {
       // Update status to let user know options were saved.
       const status = document.getElementById('status');
@@ -18,13 +21,15 @@ const saveOptions = () => {
 
 const restoreOptions = () => {
   console.log('restoreOptions')
-  chrome.storage.sync.get(['barcodeType'],
+  chrome.storage.sync.get(['barcodeType', 'setupMode'],
     (items) => {
       console.log(items)
       document.getElementById('barcode-type').value = items.barcodeType || 'datamatrix';
+      document.getElementById('setup-mode').value = items.setupMode || 'offline';
     }
   );
 };
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('barcode-type').addEventListener('change', saveOptions)
+document.getElementById('setup-mode').addEventListener('change', saveOptions)
